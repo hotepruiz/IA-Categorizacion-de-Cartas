cb1152022e1ff56c9c17d6a63a88d6066abb0328
Hotep Ruiz <titoshosito@gmail.com>
2025-04-02 20:46:12 -0600

    Ajustando archivos y reacomodando codigo

--- Codigo/FuncionesExtra.py
+++ Codigo/FuncionesExtra.py
@@ -1,15 +1,36 @@
 import cv2
 import matplotlib.pyplot as plt
 
+
+#Procesamiento imagen---------------------------------------------------------------------------------------------------------------------
 def MostrarImagen(imagen):
     if imagen is None:
         print("Error: No se pudo cargar la imagen.")
     else:
         imagen_rgb = cv2.cvtColor(imagen, cv2.COLOR_BGR2RGB)  # Convertir a RGB
         plt.imshow(imagen_rgb)
-        plt.axis("off")  # Ocultar ejes
+        plt.axis("off")  
         plt.show()
 
+    
+def DibujarCajas(imagen, resultados):
+    for resultado in resultados:
+        for box in resultado.boxes:
+            x1, y1, x2, y2 = map(int, box.xyxy[0])  # coordenadas caja
+            conf = box.conf[0].item()  # % confianza
+            label = int(box.cls[0].item())  # Clase predicha
+        
+            # Dibujar bounding box
+            cv2.rectangle(imagen, (x1, y1), (x2, y2), (0, 255, 0), 8)
+        
+            # Etiqueta con clase y confianza
+            texto = ClasesANombre(label)
+            cv2.putText(imagen, texto, (x1, y1 - 10), cv2.FONT_HERSHEY_PLAIN,3, (0, 255, 0), 5)
+
+    return imagen
+
+
+#Clases y etiquetas-------------------------------------------------------------------------------------------------------------------
 def ClasesANombre(NumeroClase: int):
     NombreClases = [
     '10 de treboles', '10 de diamantes', '10 de corazones', '10 de espadas',
@@ -25,8 +46,6 @@ def ClasesANombre(NumeroClase: int):
     'J de treboles', 'J de diamantes', 'J de corazones', 'J de espadas',
     'K de treboles', 'K de diamantes', 'K de corazones', 'K de espadas',
     'Q de treboles', 'Q de diamantes', 'Q de corazones', 'Q de espadas'
-]
+    ]
     
     return NombreClases[NumeroClase]
-    
-
--- Codigo/PredecirImagen.py
+++ Codigo/PredecirImagen.py
@@ -4,23 +4,12 @@ import matplotlib.pyplot as plt
 import FuncionesExtra 
 #modelo
 model = YOLO("Modelo entrenado/best.pt")
-imagen = cv2.imread(f"testhotep.jpeg")
+imagen = cv2.imread(f"jc.jpeg")
 
 resultados = model(imagen)
 
 # Dibujar cajas
-for resultado in resultados:
-    for box in resultado.boxes:
-        x1, y1, x2, y2 = map(int, box.xyxy[0])  # Coordenadas del bounding box
-        conf = box.conf[0].item()  # Confianza
-        label = int(box.cls[0].item())  # Clase predicha
-        
-        # Dibujar bounding box
-        cv2.rectangle(imagen, (x1, y1), (x2, y2), (0, 255, 0), 8)
-        
-        # Etiqueta con clase y confianza
-        texto = FuncionesExtra.ClasesANombre(label)
-        cv2.putText(imagen, texto, (x1, y1 - 10), cv2.FONT_HERSHEY_PLAIN,3, (0, 255, 0), 5)
+FuncionesExtra.DibujarCajas(imagen, resultados)
 
 # Mostrar imagen con detecciones
 FuncionesExtra.MostrarImagen(imagen)
